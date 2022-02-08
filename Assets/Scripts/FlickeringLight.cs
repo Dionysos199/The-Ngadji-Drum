bbfbd274aa05f040e399605432722533f4822fa1
PhilippDaube96 <philipp.daube@stud.h-da.de>
2022-02-08 23:18:09 +0100

    Added Materials and Sign

---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+
+    public bool isFlickering = false;
+    //public bool isHit = false;
+    public float timeDelay;
+
+    void Update()
+    {
+        if(isFlickering == false /*&& isHit == true*/)
+        {
+            StartCoroutine(FlickingLight());
+        }
+    }
+
+    IEnumerator FlickingLight()
+    {
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.01f, 0.5f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.5f, 1.0f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        isHit = true;
+            
+    //    }
+    //}
+}
