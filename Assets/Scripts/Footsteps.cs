59e65cfc3c88f0a6dfa42a1352d15a4cc089bfe5
PhilippDaube96 <philipp.daube@stud.h-da.de>
2022-02-09 17:28:00 +0100

    Added more flickering and materials

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    CharacterController cc;
+    AudioSource Audio;
+    ////GameObject floorTag;
+    //public Terrain terrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        Audio = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RaycastHit hit;
+        //if(Physics.Raycast(this.transform.position,Vector3.down,out hit))
+        //{
+        //    terrain = hit.collider.tag 
+        //}
+        //StartCoroutine(WalkSound());
+        if (cc.isGrounded == true && cc.velocity.magnitude > 1.0f && Audio.isPlaying == false)
+        {
+            //        //yield return new WaitForSeconds(2.0f);
+            //        //Audio.volume = Random.Range(0.8f, 1);
+            //        //Audio.pitch = Random.Range(0.8f,1.1f);
+            //Audio.Play();
+            Audio.volume = Random.Range(0.8f, 1.0f);
+            Audio.pitch = Random.Range(0.8f, 1.1f);
+            Audio.PlayDelayed(0.4f);
+
+        }
+
+    }
+
+
+
+
+
+    //public IEnumerator WalkSound() 
+    //{
+    //    if (cc.isGrounded == true && cc.velocity.magnitude > 1.0f && Audio.isPlaying == false)
+    //    {
+    //        //yield return new WaitForSeconds(2.0f);
+    //        //Audio.volume = Random.Range(0.8f, 1);
+    //        //Audio.pitch = Random.Range(0.8f,1.1f);
+    //        //Audio.Play();
+    //        Audio.PlayDelayed(0.5f);
+
+    //    }
+}
+            
+
+
+
+
