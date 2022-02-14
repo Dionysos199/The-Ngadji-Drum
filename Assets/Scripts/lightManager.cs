36309c91bb0ac311f7ae0d92cb189fd0b74316a4
ImadElKhechen <imadelkhechen@gmail.com>
2022-02-14 17:08:48 +0100

    the roots belong to phase 2

--- Assets/Scripts/lightManager.cs
+++ Assets/Scripts/lightManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class lightManager : MonoBehaviour
 {
-    public Light[] neonLightsArray;
+
     // Start is called before the first frame update
     void Start()
     {
