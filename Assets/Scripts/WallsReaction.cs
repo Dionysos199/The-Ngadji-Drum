15f645e91dbbbf78e2aed56de419112049a56d87
ImadElKhechen <imadelkhechen@gmail.com>
2022-02-15 12:16:31 +0100

    Wall explosion

--- Assets/Scripts/WallsReaction.cs
+++ Assets/Scripts/WallsReaction.cs
@@ -5,37 +5,51 @@ using UnityEngine;
 public class WallsReaction : MonoBehaviour
 
 {
+    public GameObject toDisActivate;
+    public GameObject ObjectToExplode;
     Rigidbody rb;
     public float MaxLifeTime;
     public float explosionThreshold;
-    private void Start()
+
+
+
+  
+    public void Explode(float hitForce, Vector3 u, int n)
     {
+        if(hitForce>explosionThreshold)
+        StartCoroutine(twoSteps());
 
     }
-    public void Explode(float a, Vector3 b, int c)
+    IEnumerator twoSteps()
     {
-        Debug.Log("Hitforce" + a);
-        if (a > explosionThreshold)
+
+        Debug.Log("explosion");
+        toDisActivate.SetActive(false);
+
+        float explosionMoment = Time.time;
+
+
+        yield return new WaitForEndOfFrame();
+
+        ObjectToExplode.SetActive(true);
+        Transform[] toExplodeChildren = ObjectToExplode.GetComponentsInChildren<Transform>();
+        List<GameObject> childObjects = new List<GameObject>();
+        foreach (Transform child in toExplodeChildren)
         {
-            Transform[] allChildren = GetComponentsInChildren<Transform>();
-            List<GameObject> childObjects = new List<GameObject>();
-            foreach (Transform child in allChildren)
+            childObjects.Add(child.gameObject);
+        }
+        foreach (GameObject gameObject in childObjects)
+        {
+            if (!gameObject.GetComponent<Rigidbody>())
             {
-                childObjects.Add(child.gameObject);
-            }
-            foreach (GameObject gameObject in childObjects)
-            {
-                if (!gameObject.GetComponent<Rigidbody>())
-                {
-                    gameObject.AddComponent<Rigidbody>();
-                    gameObject.AddComponent<BoxCollider>();
-                    float lifeTime = Random.Range(4, MaxLifeTime);
-                    Destroy(gameObject, lifeTime);
-                }
+                gameObject.AddComponent<Rigidbody>();
+                gameObject.AddComponent<BoxCollider>();
+                float lifeTime = Random.Range(4, MaxLifeTime);
+                Destroy(gameObject, lifeTime);
             }
         }
-    }
 
+    }
 }
 
 
